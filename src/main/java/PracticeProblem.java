67c64992ac9c611c37fab9771942e0bd42dc4ae1
JasonWu-ux <jasonwu544@gmail.com>
2026-04-06 13:45:56 +0000

    done questions

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,18 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static int towerOfHanoi(int num) {
+		if (num < 3) {
+			return -1;
+		}
+		return countmoves(num);
+	}
+	public static int countmoves(int n) {
+		if (n == 1) {
+			return 1;
+		}
+		return 2 * countmoves(n - 1) + 1;
+
+	}
 
 }
